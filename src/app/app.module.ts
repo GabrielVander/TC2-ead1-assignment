743d5e93a268066d3ac6f88a5ab06935793cae1c
Gabriel Vander <gabsvander@hotmail.com>
2020-12-11 21:05:14 -0300

    Initial layout and search input implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,18 +9,30 @@ import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { en_US } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
+import { SearchComponent } from '../components/search/search.component';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import {NzIconModule} from 'ng-zorro-antd/icon';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzTypographyModule } from 'ng-zorro-antd/typography';
 
 registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    NzInputModule,
+    NzIconModule,
+    NzButtonModule,
+    NzLayoutModule,
+    NzTypographyModule
   ],
   providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
